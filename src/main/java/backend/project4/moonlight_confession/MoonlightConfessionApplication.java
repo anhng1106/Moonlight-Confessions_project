8a3c97d04886e6f0cc726e5bc7a0ffcdfa2b2c61
anhng1106 <ntvanh1106@gmail.com>
2024-04-04 10:34:21 +0300

    first commit and add dependencies to pom.xml file

---
+++ src/main/java/backend/project4/moonlight_confession/MoonlightConfessionApplication.java
@@ -0,0 +1,13 @@
+package backend.project4.moonlight_confession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonlightConfessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonlightConfessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/project4/moonlight_confession/MoonlightConfessionApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.project4.moonlight_confession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonlightConfessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
